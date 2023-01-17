3496ba00abf7aaade27c0b7d27ae836fda791c69
isac moreira <108782511+isacmoreira10@users.noreply.github.com>
2023-01-17 20:20:59 -0300

    Add files via upload

---
+++ Sum/script.js
@@ -0,0 +1,20 @@
+
+function calculate(){
+    var number1 = document.querySelector('#number1').value;
+        number1 = parseInt(number1);
+    var number2 = document.querySelector('#number2').value;
+        number2 = parseInt(number2);
+    var output =   (number1 + number2) ;
+        document.querySelector('#output').innerHTML = output;
+}
+
+
+
+
+
+
+
+
+
+
+
